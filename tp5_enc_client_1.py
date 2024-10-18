1e9df1aec332ed2708836608048aee84427b0cc8
hyanice <yanice.hourcade@ynov.com>
2024-10-18 10:02:36 +0200

    start

--- tp5_enc_client_1.py
+++ tp5_enc_client_1.py
@@ -1,5 +1,20 @@
 import socket
+import re
 
+def verif_input(msg):
+    pattern = r"^\s*(-?\d+)\s*([\+\-\*])\s*(-?\d+)\s*$"
+    match = re.match(pattern, msg)
+    
+    if not match:
+        print("Format incorrect. Utilisez le format: nombre opérateur nombre (ex: 3 + 3)")
+        exit()
+    
+    x, operator, y = match.groups()
+    
+    if not (-1048575 <= int(x) <= 1048575 and -1048575 <= int(y) <= 1048575):
+        print("Les nombres doivent être compris entre -1048575 et 1048575")
+        exit()
+        
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('10.10.10.11', 13337))
 s.send('Hello'.encode())
@@ -9,6 +24,8 @@ data = s.recv(1024)
 
 # Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
+
+verif_input(msg)
 
 # On envoie
 s.send(msg.encode())
